1fd930cd39d8a7ec632644c6abf9436939b03e43
jiangchen <jiangchen23@qq.com>
2022-02-20 02:56:55 +0800

    合并k个有序列表

--- src/algs/list/0023.merge-k-sorted-lists/merge_k_sorted_lists.go
+++ src/algs/list/0023.merge-k-sorted-lists/merge_k_sorted_lists.go
@@ -5,27 +5,48 @@ import (
 )
 
 type ListNode = data_structure.ListNode
-type PriorityQueue = data_structure.PriorityQueue
 
-func mergeKSortedLists(lists []*ListNode) *ListNode {
-	if len(lists) == 0 {
+func mergeKSortedLists1(lists []*ListNode) *ListNode {
+	size := len(lists)
+	if size == 0 {
 		return nil
 	}
-	dummy := ListNode{}
-	//p := dummy
-	pq := PriorityQueue{}
-	for i := 0; i < len(lists); i++ {
-		pq.Push(lists[i])
+	if size == 1 {
+		return lists[0]
 	}
-	for {
-		if len(pq) != 0 {
-			//node := pq.Pop()
-			//p.Next = node
-			//if node.Next != nil {
-			//	pq.Push(node.Next)
-			//}
-			//p = *p.Next
-		}
+	var tmp *ListNode
+	for _, elem := range lists {
+		tmp = mergeTwoSortedList(tmp, elem)
 	}
-	return dummy.Next
+	return tmp
+}
+
+// 方法二：使用归并算法
+func mergeKSortedLists2(lists []*ListNode) *ListNode {
+	size := len(lists)
+	if size == 0 {
+		return nil
+	}
+	if size == 1 {
+		return lists[0]
+	}
+	num := size / 2
+	left := mergeKSortedLists2(lists[:num])
+	right := mergeKSortedLists2(lists[num:])
+	return mergeTwoSortedList(left, right)
+}
+
+func mergeTwoSortedList(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoSortedList(list1.Next, list2)
+		return list1
+	}
+	list2.Next = mergeTwoSortedList(list1, list2.Next)
+	return list2
 }
--- src/algs/list/0023.merge-k-sorted-lists/merge_k_sorted_lists_test.go
+++ src/algs/list/0023.merge-k-sorted-lists/merge_k_sorted_lists_test.go
@@ -9,7 +9,8 @@ func TestMergeTwoSortedLists(t *testing.T) {
 	list1 := data_structure.NewListNodeFromArr([]int{1, 4, 7})
 	list2 := data_structure.NewListNodeFromArr([]int{2, 5, 8})
 	list3 := data_structure.NewListNodeFromArr([]int{3, 6, 9})
-	lists := []ListNode{*list1, *list2, *list3}
-	mergedList := mergeKSortedLists(lists)
+	var lists []*ListNode
+	tmp := append(lists, list1, list2, list3)
+	mergedList := mergeKSortedLists2(tmp)
 	println(data_structure.GetListNodeStr(mergedList))
 }

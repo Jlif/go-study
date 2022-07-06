5eedf6cb1d9d0ab8dd324d392f4df0a5afa6bb12
jiang chen <jiangchen23@qq.com>
2022-07-07 00:35:55 +0800

    数组-字符串排列

---
+++ src/algs/array/0576.permutation-in-string/permutation_in_string.go
@@ -0,0 +1,53 @@
+package _576_permutation_in_string
+
+/**
+给你两个字符串s1和s2 ，写一个函数来判断 s2 是否包含 s1的排列。如果是，返回 true ；否则，返回 false 。
+
+换句话说，s1 的排列之一是 s2 的 子串 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/permutation-in-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func checkInclusion(s1 string, s2 string) bool {
+	set := make(map[rune]int)
+	window := make(map[rune]int)
+	for _, v := range []rune(s1) {
+		set[v] = 0
+	}
+
+	left, right, valid := 0, 0, 0
+	src := []rune(s2)
+
+	for right < len(src) {
+		r := src[right]
+		right++
+		//进行窗口内数据的一系列更新
+		if _, exist := set[r]; exist {
+			window[r]++
+			if window[r] == set[r] {
+				valid++
+			}
+		}
+
+		//判断左侧窗口是否要收缩
+		for right-left >= len(set) {
+			//在这里判断是否找到了合法子串
+			if valid == len(set) {
+				return true
+			}
+			d := src[left]
+			left++
+			//进行窗口内的一系列数据更新
+			if _, exist := set[d]; exist {
+				if window[d] == set[d] {
+					valid--
+				}
+				window[d]--
+			}
+		}
+	}
+
+	return false
+}
---
+++ src/algs/array/0576.permutation-in-string/permutation_in_string_test.go
@@ -0,0 +1,33 @@
+package _576_permutation_in_string
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type question struct {
+	param
+	ans
+}
+
+type param struct {
+	s1 string
+	s2 string
+}
+
+type ans struct {
+	ans bool
+}
+
+func Test(t *testing.T) {
+
+	cases := []question{
+		{param{"ab", "eidbaooo"}, ans{true}},
+		{param{"ab", "eidboaoo"}, ans{false}},
+	}
+
+	for _, v := range cases {
+		assert.New(t).Equal(v.ans.ans, checkInclusion(v.param.s1, v.param.s2))
+	}
+
+}

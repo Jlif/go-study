aa238028251894df21b8f04fd38fc590c2f0816a
蒋琛 <jiangchen2046@gmail.com>
2022-07-14 00:38:26 +0800

    滑动窗口-找到字符串中所有字符异位词

--- src/algs/array/0438.find-all-anagrams-in-a-string/find_all_anagrams_in_a_string.go
+++ src/algs/array/0438.find-all-anagrams-in-a-string/find_all_anagrams_in_a_string.go
@@ -1,6 +1,28 @@
 package _438_find_all_anagrams_in_a_string
 
 func findAnagrams(s string, p string) []int {
+	set := make(map[byte]int)
+	window := make(map[byte]int)
+	for _, v := range []byte(s) {
+		set[v]++
+	}
+
+	left, right, valid := 0, 0, 0
+
+	res := make([]int, 4)
+
+	for right < len(s) {
+		b := s[right]
+		right++
+
+		if _, exist := set[b]; exist {
+			window[b]++
+			if window[b] == set[b] {
+				valid++
+			}
+		}
+
+	}
 
 	return nil
 }

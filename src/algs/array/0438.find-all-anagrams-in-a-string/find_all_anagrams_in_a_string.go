878628b30d193e1362d6deae63aa1933dd307718
蒋琛 <jiangchen2046@gmail.com>
2022-07-16 16:31:56 +0800

    滑动窗口-找到字符串中所有字符异位词

--- src/algs/array/0438.find-all-anagrams-in-a-string/find_all_anagrams_in_a_string.go
+++ src/algs/array/0438.find-all-anagrams-in-a-string/find_all_anagrams_in_a_string.go
@@ -1,28 +1,46 @@
 package _438_find_all_anagrams_in_a_string
 
+/**
+给定两个字符串s和 p，找到s中所有p的异位词的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+
+异位词 指由相同字母重排列形成的字符串（包括相同的字符串）
+*/
+
 func findAnagrams(s string, p string) []int {
 	set := make(map[byte]int)
 	window := make(map[byte]int)
-	for _, v := range []byte(s) {
+	for _, v := range []byte(p) {
 		set[v]++
 	}
 
 	left, right, valid := 0, 0, 0
 
-	res := make([]int, 4)
+	var res []int
 
 	for right < len(s) {
-		b := s[right]
-		right++
-
-		if _, exist := set[b]; exist {
-			window[b]++
-			if window[b] == set[b] {
+		rs := s[right]
+		if _, exist := set[rs]; exist {
+			window[rs]++
+			if window[rs] == set[rs] {
 				valid++
 			}
 		}
+		right++
 
+		for right-left >= len(p) {
+			if valid == len(set) {
+				res = append(res, left)
+			}
+			ls := s[left]
+			if _, exist := set[ls]; exist {
+				if window[ls] == set[ls] {
+					valid--
+				}
+				window[ls]--
+			}
+			left++
+		}
 	}
 
-	return nil
+	return res
 }

e2ed08b18bebf1468be8de8eb44101849538d82c
蒋琛 <jiangchen2046@gmail.com>
2022-07-21 00:59:29 +0800

    34-在排序数组中查找元素的第一个跟最后一个位置

--- src/algs/array/0034.find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_element_in_sorted_array.go
+++ src/algs/array/0034.find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_element_in_sorted_array.go
@@ -1,6 +1,55 @@
 package _034_find_first_and_last_position_of_element_in_sorted_array
 
 func searchRange(nums []int, target int) []int {
+	return []int{findFirst(nums, target), findLast(nums, target)}
+}
 
-	return nil
+func findFirst(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] == target {
+			right = mid - 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+
+	if left == len(nums) {
+		return -1
+	}
+
+	if nums[left] == target {
+		return left
+	} else {
+		return -1
+	}
+}
+
+func findLast(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+
+	if left == 0 {
+		return -1
+	}
+
+	if nums[left-1] == target {
+		return left - 1
+	} else {
+		return -1
+	}
 }

dcfd5473b5f7e4638aa44d73a4ef90908e1befff
jiangchen <cjiang@86links.com>
2022-09-08 18:44:00 +0800

    870-优势洗牌

--- src/algs/array/0870.advantage-shuffle/advantage_shuffle.go
+++ src/algs/array/0870.advantage-shuffle/advantage_shuffle.go
@@ -1,33 +1,58 @@
 package _870_advantage_shuffle
 
 import (
-	"fmt"
 	"sort"
 )
 
+/**
+给定两个大小相等的数组nums1和nums2，nums1相对于 nums的优势可以用满足nums1[i] > nums2[i]的索引 i的数目来描述。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/advantage-shuffle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 func advantageCount(nums1 []int, nums2 []int) []int {
+	n := len(nums1)
 
-	fmt.Println(nums1)
-	fmt.Println(nums2)
+	tmp := make(itemSlice, n)
+	for index, val := range nums2 {
+		tmp[index] = item{index: index, value: val}
+	}
 
+	//s1升序
 	sort.Ints(nums1)
-	sort.Sort(sort.Reverse(sort.IntSlice(nums2)))
-
-	fmt.Println(nums1)
-	fmt.Println(nums2)
+	//s2降序
+	sort.Sort(tmp)
 
 	left := 0
-	right := len(nums1) - 1
-	var res []int
-
-	for index, num := range nums2 {
-		if num >= nums1[index] {
-			res[index] = nums1[left]
+	right := n - 1
+	res := make([]int, n)
+	for _, num := range tmp {
+		if num.value >= nums1[right] {
+			res[num.index] = nums1[left]
 			left++
 		} else {
-			res[index] = nums1[right]
+			res[num.index] = nums1[right]
 			right--
 		}
 	}
 	return res
+}
+
+type item struct {
+	index int
+	value int
+}
+
+type itemSlice []item
+
+func (t itemSlice) Len() int {
+	return len(t)
+}
+func (t itemSlice) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+func (t itemSlice) Less(i, j int) bool {
+	return t[j].value < t[i].value
 }

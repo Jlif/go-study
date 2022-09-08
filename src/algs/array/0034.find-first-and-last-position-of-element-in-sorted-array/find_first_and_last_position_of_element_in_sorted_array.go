eb3f2d62c69e74d396cb7f286ac56b560d9edb7e
jiangchen <cjiang@86links.com>
2022-09-08 16:21:51 +0800

    添加注释

--- src/algs/array/0034.find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_element_in_sorted_array.go
+++ src/algs/array/0034.find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_element_in_sorted_array.go
@@ -1,5 +1,17 @@
 package _034_find_first_and_last_position_of_element_in_sorted_array
 
+/**
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回[-1, -1]。
+
+你必须设计并实现时间复杂度为O(log n)的算法解决此问题。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 func searchRange(nums []int, target int) []int {
 	return []int{findFirst(nums, target), findLast(nums, target)}
 }

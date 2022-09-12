74dc3666cb0ec9c4efc2b3638f8fa47a9ef37651
蒋琛 <jiangchen2046@gmail.com>
2022-09-12 23:03:02 +0800

    710-黑名单中的随机数

---
+++ src/algs/array/0710.random_pick_with_blacklist/random_pick_with_blacklist.go
@@ -0,0 +1,45 @@
+package _710_random_pick_with_blacklist
+
+import "math/rand"
+
+/*
+*
+给定一个整数 n 和一个 无重复 黑名单整数数组blacklist。设计一种算法，从 [0, n - 1] 范围内的任意整数中选取一个未加入黑名单blacklist的整数。
+任何在上述范围内且不在黑名单blacklist中的整数都应该有 同等的可能性 被返回。
+
+优化你的算法，使它最小化调用语言 内置 随机函数的次数。
+
+实现Solution类:
+
+Solution(int n, int[] blacklist)初始化整数 n 和被加入黑名单blacklist的整数
+int pick()返回一个范围为 [0, n - 1] 且不在黑名单blacklist 中的随机整数
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/random-pick-with-blacklist
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+type Solution struct {
+	w []int
+}
+
+func Constructor(n int, blacklist []int) Solution {
+	w := make([]int, n-len(blacklist))
+	blackMap := map[int]struct{}{}
+	for _, val := range blacklist {
+		blackMap[val] = struct{}{}
+	}
+	tmp := 0
+	for i := 0; i < n; i++ {
+		if _, exist := blackMap[i]; exist {
+			continue
+		}
+		w[tmp] = i
+		tmp++
+	}
+	return Solution{w}
+}
+
+func (this *Solution) Pick() int {
+	return this.w[rand.Intn(len(this.w))]
+}
